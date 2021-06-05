d543aba1ddd7bd965a98dba78c2ab5b5283dee0a
khanahmedm <81051236+khanahmedm@users.noreply.github.com>
2021-06-04 22:33:02 -0500

    Add files via upload

---
+++ factorical.py
@@ -0,0 +1,10 @@
+# This program calculates factorials
+# by calling the same function recursively
+
+def factorial(n):
+    if n <= 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print(factorial(5))
